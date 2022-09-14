185e40b9cf911ffa1fb15e2bda2d0f543012a0dd
garriton <52850638+garriton@users.noreply.github.com>
2022-09-14 23:59:04 +0800

    打包

--- app.py
+++ app.py
@@ -1,10 +1,73 @@
-import requests
 import wx
 import wx.lib.buttons as buts
 import wx.lib.scrolledpanel as scrolled
 import copy
+import os
 
-from utils import TILES, count_to_tiles, tiles_to_count, YIZHONG
+#from utils import TILES, count_to_tiles, tiles_to_count, YIZHONG
+TILES = [
+    "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m",
+    "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p",
+    "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s",
+    "1z", "2z", "3z", "4z", "5z", "6z", "7z",
+]
+
+YIZHONG = {
+    "役满": 9,
+    "双倍役满": 1,
+    "六番": 1,
+    "三番": 3,
+    "二番": 10,
+    "一番": 1,
+}
+
+
+def count_to_tiles(cnt):
+    if len(cnt) != len(TILES):
+        print("cnt 长度必须为", len(TILES))
+        return ""
+
+    tiles = ""
+    for i, type_ in enumerate(['m', 'p', 's', 'z']):
+        wrote = False
+        for j in range(9):
+            idx = 9 * i + j
+            if idx >= len(TILES):
+                break
+            for k in range(cnt[idx]):
+                tiles += str(j + 1)
+                wrote = True
+        if wrote:
+            tiles += type_ + " "
+    return tiles.strip()
+
+
+def tiles_to_count(tiles):
+    tiles = tiles.strip()
+    if tiles == "":
+        return []
+
+    cnt = [0] * len(TILES)
+
+    for split in tiles.split():
+        split = split.strip()
+        if len(split) <= 1:
+            print("参数错误:", split)
+            return []
+        for c in split[:-1]:
+            tile = c + split[-1]
+            try:
+                index = TILES.index(tile)
+            except ValueError:
+                print("参数错误:", tile)
+                return []
+            cnt[index] += 1
+            if cnt[index] > 4:
+                print("参数错误: 超过4张一样的牌")
+                return []
+
+    return cnt
+
 # TODO: data binding
 cnt = [0] * len(TILES)
 
@@ -1004,3 +1067,5 @@ if __name__ == '__main__':
     frame.Show()
 
     app.MainLoop()
+
+    os.system('pause')
